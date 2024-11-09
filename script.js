e65e2943262e978f4031b404ec50ce4fc4221707
rishabhy043 <rishabh.knipss043@gmail.com>
2024-11-09 12:00:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const qrText = document.getElementById('qr-text');
+const sizes = document.getElementById('sizes');
+const generateBtn = document.getElementById('generateBtn');
+const downloadBtn = document.getElementById('downloadBtn');
+const qrContainer = document.querySelector('.qr-body');
